5f01f8fdc2aa756e461ff5be7539a3af4def9814
akhileshgoud <akhilesh.gounikadi@xcubelabs.com>
2022-11-30 12:32:50 +0530

    [TASK]: interpolation

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,9 @@
 import { NgModule } from "@angular/core";
 import { BrowserModule } from "@angular/platform-browser";
 import { AppComponent } from "./app.component";
+import { BooksComponent } from './books/books.component';
 @NgModule({
-    declarations : [AppComponent],
+    declarations : [AppComponent, BooksComponent],
     imports : [BrowserModule],
     bootstrap : [AppComponent]
 })
---
+++ src/app/books/books.component.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-books',
+  templateUrl: './books.component.html',
+  styleUrls: ['./books.component.css']
+})
+export class BooksComponent {
+  name1 = 'white pouch'
+  author1 = 'raghuvaran'
+  src1 = "https://m.media-amazon.com/images/I/51bzAAbVPrL._SY879_.jpg"
+  name2 = "black pouch"
+  author2 = 'raghuvaran'
+  src2 = "https://m.media-amazon.com/images/I/61exsXR8IxL._SX679_.jpg"
+}
