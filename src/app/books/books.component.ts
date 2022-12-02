b419b5729be04614c5d551635fc587fd42b5dd46
akhileshgoud <akhilesh.gounikadi@xcubelabs.com>
2022-12-02 23:04:06 +0530

    [TASK]: styles

--- src/app/books/books.component.ts
+++ src/app/books/books.component.ts
@@ -24,6 +24,18 @@ export class BooksComponent {
       author:  'amazon',
       image: "https://m.media-amazon.com/images/I/61exsXR8IxL._SX679_.jpg",
       amount: 1200
+    },
+    {
+      name: 'white pouch',
+      author: 'flipkart',
+      image:"https://m.media-amazon.com/images/I/51bzAAbVPrL._SY879_.jpg",
+      amount: 600
+    },
+    {
+      name:  "black pouch",
+      author: 'flipkart',
+      image: "https://m.media-amazon.com/images/I/61exsXR8IxL._SX679_.jpg",
+      amount: 1100
     }
   ]
   isDisabled=false 
